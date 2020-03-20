cdf9404e8710d81896ab4008798f9488599ffae3
nsbt <55523468+nsbt@users.noreply.github.com>
2020-03-20 21:21:27 +0900

    add query param

--- src/main/kotlin/com/bukubako/api/controller/SearchController.kt
+++ src/main/kotlin/com/bukubako/api/controller/SearchController.kt
@@ -1,13 +1,12 @@
 package com.bukubako.api.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SearchController {
 
     @GetMapping("search")
-    fun get(): String {
-        return "Search API called."
-    }
+    fun get(@RequestParam("q") q: String): String = "Search API called. Keyword is ${q}"
 }
