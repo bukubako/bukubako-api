a6982fe8c071935bcf67ae81af3099767b4ca96e
nsbt <55523468+nsbt@users.noreply.github.com>
2020-03-22 19:07:10 +0900

    add items to search resource

--- src/main/kotlin/com/bukubako/api/controller/SearchController.kt
+++ src/main/kotlin/com/bukubako/api/controller/SearchController.kt
@@ -1,5 +1,6 @@
 package com.bukubako.api.controller
 
+import com.bukubako.api.controller.response.ItemResponse
 import com.bukubako.api.controller.response.SearchResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,5 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 class SearchController {
 
     @GetMapping("search")
-    fun get(@RequestParam("q") q: String): SearchResponse = SearchResponse(q, 0)
+    fun get(@RequestParam("q") q: String): SearchResponse =
+            SearchResponse(
+                    q,
+                    2,
+                    listOf(
+                            ItemResponse("タイトル1", "説明1"),
+                            ItemResponse("タイトル2", "説明2")
+                    )
+            )
 }
--- src/main/kotlin/com/bukubako/api/controller/response/SearchResponse.kt
+++ src/main/kotlin/com/bukubako/api/controller/response/SearchResponse.kt
@@ -2,5 +2,10 @@ package com.bukubako.api.controller.response
 
 data class SearchResponse(
         val keyword: String,
-        val totalItems: Int) {
-}
+        val totalItems: Int,
+        val items: List<ItemResponse>)
+
+data class ItemResponse(
+        val title: String,
+        val description: String
+)
