d43c61e26a6d3dbed71865ec1adad801ea689342
nsbt <55523468+nsbt@users.noreply.github.com>
2020-04-13 22:13:46 +0900

    add swagger config

---
+++ src/main/kotlin/com/bukubako/api/config/SwaggerConfig.kt
@@ -0,0 +1,27 @@
+package com.bukubako.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun bukubakoApi(): Docket =
+            Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .paths(PathSelectors.ant("/search"))
+                    .build()
+                    .useDefaultResponseMessages(false)
+                    .host("springbootswagger.example.com")
+                    .apiInfo(ApiInfoBuilder()
+                            .title("Bukubako API")
+                            .description("This provides APIs for Bukubako")
+                            .version("1.0.0")
+                            .build())
+}
