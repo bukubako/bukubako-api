c101ddf02152dd2422c5f1837a8051905de87621
nsbt <55523468+nsbt@users.noreply.github.com>
2020-05-06 15:11:51 +0900

    add logic to format published date

--- src/main/kotlin/com/bukubako/api/domain/PublishedDate.kt
+++ src/main/kotlin/com/bukubako/api/domain/PublishedDate.kt
@@ -8,6 +8,18 @@ import java.time.temporal.TemporalAccessor
 
 class PublishedDate(private val string: String) {
 
-    fun value(): String = string
+    fun value(): String =
+            when {
+                string.matches(Regex("\\d{4}-\\d{2}-\\d{2}")) ->
+                    LocalDate.parse(string).format(DateTimeFormatter.ofPattern("yyyy年MM月dd日"))
 
+                string.matches(Regex("\\d{4}-\\d{2}")) ->
+                    YearMonth.parse(string).format(DateTimeFormatter.ofPattern("yyyy年MM月"))
+
+                string.matches(Regex("\\d{4}")) ->
+                    Year.parse(string).format(DateTimeFormatter.ofPattern("yyyy年"))
+
+                else ->
+                    throw IllegalArgumentException("年月日の形式が不正: $string")
+            }
 }
